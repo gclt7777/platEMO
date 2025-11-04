3d8779a8c3091cc0028c73b2dbda9a01fb75d25a
gclt7777 <zhaoxinman07@163.com>
2025-11-04 16:39:45 +0800

    Update LSMaODE for current PlatEMO interface

--- PlatEMO/Algorithms/Multi-objective optimization/self/LSMaODE/CalDomcount.m
+++ PlatEMO/Algorithms/Multi-objective optimization/self/LSMaODE/CalDomcount.m
@@ -1,16 +1,19 @@
-function Domcount = CalDomcount(PopObj,input_pop)
+function Domcount = CalDomcount(PopObj,input_obj)
+% Count the number of solutions in PopObj that dominate the input object.
 
+    if isempty(PopObj)
+        Domcount = 0;
+        return;
+    end
 
     N = size(PopObj,1);
-
     Dominate = false(1,N);
-        for j = 1 : N
-            k = any(input_pop<PopObj(j,:)) - any(input_pop>PopObj(j,:));
-            if k == -1
-                Dominate(j) = true;
-            end
+    for j = 1 : N
+        better = any(input_obj < PopObj(j,:));
+        worse  = any(input_obj > PopObj(j,:));
+        if ~better && worse
+            Dominate(j) = true;
         end
- 
-    
+    end
     Domcount = sum(Dominate);
 end
--- PlatEMO/Algorithms/Multi-objective optimization/self/LSMaODE/MED.m
+++ PlatEMO/Algorithms/Multi-objective optimization/self/LSMaODE/MED.m
@@ -1,22 +1,36 @@
-    function  [dis_y_par,dis_y_chi] = MED(Population,par,child)
-  
-        Population_obj=Population.objs;
-        Population_dec=Population.decs;
-        child_obj=child.objs;
-        child_dec=child.decs;
-        N=length(Population);
+function  [dis_y_par,dis_y_chi] = MED(Population,par,child)
+% Calculate the multiplicative Euclidean distance used in LSMaODE.
 
-        %%%%%%child
-        dist_y =  pdist2(Population_obj, child_obj);
-        [dis_k_y,~]=sort(dist_y);
-        sum_y=sum(dis_k_y);
-        neardist_y_chi=dis_k_y(1);
-        dis_y_chi=sum_y*neardist_y_chi;
-        %%%%%%par
-        par_obj=par.objs;       
-        dist_y1 =  pdist2(Population_obj, par_obj);
-        [dis_k_y1,~]=sort(dist_y1);
-        sum_y1=sum(dis_k_y1);
-        neardist_y_par=dis_k_y1(1);
-        dis_y_par=sum_y1*neardist_y_par;
+    if isempty(Population)
+        dis_y_par = 0;
+        dis_y_chi = 0;
+        return;
     end
+
+    Population_obj = Population.objs;
+    child_obj      = child.objs;
+    par_obj        = par.objs;
+
+    dist_child = sqrt(sum((Population_obj - child_obj).^2,2));
+    dist_par   = sqrt(sum((Population_obj - par_obj).^2,2));
+
+    dist_child = sort(dist_child(:));
+    dist_par   = sort(dist_par(:));
+
+    sum_child = sum(dist_child);
+    sum_par   = sum(dist_par);
+
+    if isempty(dist_child)
+        near_child = 0;
+    else
+        near_child = dist_child(1);
+    end
+    if isempty(dist_par)
+        near_par = 0;
+    else
+        near_par = dist_par(1);
+    end
+
+    dis_y_chi = sum_child * near_child;
+    dis_y_par = sum_par * near_par;
+end
