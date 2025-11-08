62e6068d969344a3ebff970ecea8cd14a80aa494
gclt7777 <zhaoxinman07@163.com>
2025-11-08 16:20:36 +0800

    Embed variable clustering into EAGO/A classes

--- PlatEMO/Algorithms/Multi-objective optimization/self/EAGOA/EAGOA.m
+++ PlatEMO/Algorithms/Multi-objective optimization/self/EAGOA/EAGOA.m
@@ -1,284 +1,280 @@
-function EAGOA(Global)
-% <algorithm> <L>
+classdef EAGOA < ALGORITHM
+    % <2023> <multi/many> <real/integer/label/binary/permutation> <large>
+    % Enhanced Objective Space-based Adaptive Optimization (EAGOA)
 
+    methods
+        function main(Algorithm,Problem)
+            %% Parameter setting
+            [nSel,nPer,~,~] = Algorithm.ParameterSet(5,50,5,1);
 
-%------------------------------- Copyright --------------------------------
-% Copyright (c) 2018-2019 BIMK Group. You are free to use the PlatEMO for
-% research purposes. All publications which use this platform or any code
-% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
-% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
-% for evolutionary multi-objective optimization [educational forum], IEEE
-% Computational Intelligence Magazine, 2017, 12(4): 73-87".
-%--------------------------------------------------------------------------
+            %% Generate random population
+            Population = Problem.Initialization();
+            N = length(Population);
+            D = size(Population.decs,2);
 
-
-    [nSel,nPer,nCor,type] = Global.ParameterSet(5,50,5,1);
-
-    Population = Global.Initialization();
-    N = length(Population);
-    D = size(Population.decs, 2);
-    M = size(Population.objs, 2);
-
-    
-    
-
-    R = Population.objs\Population.decs;
-	
-	[m1,m2] = VariableClustering_A(Global,Population,nSel,nPer);
-	%m1 = [1:D];
-	%m2 = [1:D];
- 
- 
-
-          if length(m1) ~= 0
-
-             m1; 
-          else
-             m1 = uint16(rand(1,D/50) * (D - 1) + 1);
-          end
-
-    
-    while Global.NotTermination(Population)
-
-
-            
-          
-         if Global.evaluated / Global.evaluation < 0.9
-                temp = 5;
-         else
-                temp = 1;
-         end
-          
-        
-        
-         for j = 1 : temp*2
-            for ij = 1 : length(m2)
-                drawnow();
-
-				Population = SingleOptimization(Population,m2(ij), R,Global);
-
+            R = Population.objs\Population.decs;
+            [m1,m2] = EAGOA.VariableClustering(Problem,Population,nSel,nPer);
+            if isempty(m1)
+                count = max(1,ceil(D/50));
+                m1    = uint16(rand(1,count)*(D-1)+1);
             end
-         end
-            
-        for j = 1 : temp
-            
-            drawnow();
-            Population = GroupOptimization(Population,m1, R,Global);
 
-        end                 
+            while Algorithm.NotTerminated(Population)
+                if Problem.FE / Problem.maxFE < 0.9
+                    temp = 5;
+                else
+                    temp = 1;
+                end
 
-           
+                for j = 1 : temp*2
+                    if Problem.FE >= Problem.maxFE
+                        break;
+                    end
+                    for ij = 1 : length(m2)
+                        if Problem.FE >= Problem.maxFE
+                            break;
+                        end
+                        drawnow();
+                        Population = EAGOA.SingleOptimization(Problem,Population,m2(ij),R);
+                    end
+                end
 
-        
-        %R = Population.objs\Population.decs;
+                for j = 1 : temp
+                    if Problem.FE >= Problem.maxFE
+                        break;
+                    end
+                    drawnow();
+                    Population = EAGOA.GroupOptimization(Problem,Population,m1,R);
+                end
+            end
+        end
     end
-    
-    
+
+    methods(Static, Access = private)
+        function [PV,DV] = VariableClustering(Problem,Population,nSel,nPer)
+            [N,D] = size(Population.decs);
+            ND    = NDSort(Population.objs,1) == 1;
+            fmin  = min(Population(ND).objs,[],1);
+            fmax  = max(Population(ND).objs,[],1);
+            if any(fmax==fmin)
+                fmax = ones(size(fmax));
+                fmin = zeros(size(fmin));
+            end
+
+            Angle  = zeros(D,nSel);
+            RMSE   = zeros(D,nSel);
+            Sample = randi(N,1,nSel);
+            for i = 1 : D
+                drawnow();
+                if Problem.maxFE - Problem.FE < nSel*nPer
+                    break;
+                end
+                Decs      = repmat(Population(Sample).decs,nPer,1);
+                Decs(:,i) = unifrnd(Problem.lower(i),Problem.upper(i),size(Decs,1),1);
+                newPopu   = Problem.Evaluation(Decs);
+                if length(newPopu) < nSel*nPer
+                    break;
+                end
+                for j = 1 : nSel
+                    Points = newPopu(j:nSel:end).objs;
+                    Points = (Points-repmat(fmin,size(Points,1),1))./repmat(fmax-fmin,size(Points,1),1);
+                    Points = Points - repmat(mean(Points,1),nPer,1);
+                    [~,~,V] = svd(Points);
+                    Vector  = V(:,1)'./norm(V(:,1)');
+                    error = zeros(1,nPer);
+                    for k = 1 : nPer
+                        error(k) = norm(Points(k,:)-sum(Points(k,:).*Vector)*Vector);
+                    end
+                    RMSE(i,j) = sqrt(sum(error.^2));
+                    normal     = ones(1,size(Vector,2));
+                    sine       = abs(sum(Vector.*normal,2))./norm(Vector)./norm(normal);
+                    Angle(i,j) = real(asin(sine)/pi*180);
+                end
+            end
+
+            VariableKind = (mean(RMSE,2)<1e-2)';
+            result       = kmeans(Angle,2)';
+            if any(result(VariableKind)==1) && any(result(VariableKind)==2)
+                if mean(mean(Angle(result==1&VariableKind,:))) > mean(mean(Angle(result==2&VariableKind,:)))
+                    VariableKind = VariableKind & result==1;
+                else
+                    VariableKind = VariableKind & result==2;
+                end
+            end
+            PV = find(~VariableKind);
+            DV = find(VariableKind);
+        end
+
+        function Population2 = GroupOptimization(Problem,Population,Div_V,R)
+            N = length(Population);
+            if isempty(Div_V)
+                Population2 = Population;
+                return;
+            end
+            if Problem.FE >= Problem.maxFE
+                Population2 = Population;
+                return;
+            end
+            OffDec = Population(TournamentSelection(2,N,EAGOA.calCon(Population.objs))).decs;
+            NewObjs  = EAGOA.GAhalf3(Population.objs,N);
+            Offspring_Convergence  = EAGOA.ELU(NewObjs * R(:,Div_V));
+            NewObjs2 = EAGOA.GAhalf2_2(Population.objs,N);
+            Offspring_Convergence2 = EAGOA.ELU(NewObjs2 * R(:,Div_V));
+            temp  = sqrt(OffDec(randperm(N),Div_V).*OffDec(randperm(N),Div_V));
+            temp2 = OffDec(:,Div_V);
+            a     = randperm(N);
+            temp(temp>OffDec(a,Div_V)) = temp2(temp>OffDec(a,Div_V));
+            OffDec(:,Div_V) = temp + (Offspring_Convergence-Offspring_Convergence2);
+            OffDec(:,Div_V) = min(max(OffDec(:,Div_V),repmat(Problem.lower(Div_V),N,1)),repmat(Problem.upper(Div_V),N,1));
+
+            [Offspring,~] = EAGOA.EvaluateWithBudget(Problem,OffDec);
+            if isempty(Offspring)
+                Population2 = Population;
+                return;
+            end
+            Population2 = EnvironmentalSelection_A([Population,Offspring],N);
+        end
+
+        function Population = SingleOptimization(Problem,Population,con_V,R)
+            [N,~] = size(Population.decs);
+            if isempty(con_V)
+                return;
+            end
+            if Problem.FE >= Problem.maxFE
+                return;
+            end
+            OffDec = Population.decs;
+
+            NewObjs  = EAGOA.GAhalf3(Population(randperm(N)).objs,N);
+            Offspring_Convergence  = EAGOA.FL(NewObjs * R(:,con_V));
+            NewObjs2 = EAGOA.GAhalf2_2(Population(randperm(N)).objs,N);
+            Offspring_Convergence2 = EAGOA.FL(NewObjs2 * R(:,con_V));
+
+            OffDec(:,con_V) = OffDec(randperm(N),con_V) + (Offspring_Convergence-Offspring_Convergence2);
+            OffDec(:,con_V) = min(max(OffDec(:,con_V),repmat(Problem.lower(con_V),N,1)),repmat(Problem.upper(con_V),N,1));
+
+            remain = Problem.maxFE - Problem.FE;
+            if remain <= 0
+                return;
+            elseif remain < N
+                evalIdx = randperm(N,remain);
+            else
+                evalIdx = 1:N;
+            end
+            [Offspring,evalIdx] = EAGOA.EvaluateWithBudget(Problem,OffDec,evalIdx);
+            if isempty(evalIdx)
+                return;
+            end
+
+            updated = sum(Offspring.objs <= Population(evalIdx).objs,2) >= Problem.M;
+            Population(evalIdx(updated)) = Offspring(updated);
+        end
+
+        function Offspring = GAhalf2(Parent,~)
+            Offspring = Parent - Parent.*(rand(size(Parent))*2 - 1)*0.5;
+        end
+
+        function output = sigmoid(x)
+            output = 1./(1+exp(-x));
+        end
+
+        function output = swish(x)
+            output = x./(1+exp(-x));
+        end
+
+        function output = ELU(x)
+            output = 0.1*(exp(x)-1);
+        end
+
+        function output = FL(x)
+            output = x;
+            output(x>=0) = 0.5*output(x>=0);
+        end
+
+        function Offspring = GAhalf4(Parent,~)
+            Offspring = Parent - Parent.*(rand(size(Parent))-0.5);
+        end
+
+        function Offspring = GAhalf3(Parent,~)
+            [proC,disC,proM,disM] = deal(1,20,1,20);
+            lower = min(Parent,[],1)*0.01;
+            upper = max(Parent,[],1)*10;
+            Parent1 = Parent(1:floor(end/2),:);
+            Parent2 = Parent(floor(end/2)+1:floor(end/2)*2,:);
+            [Nsize,D] = size(Parent1);
+            beta = zeros(Nsize,D);
+            mu   = rand(Nsize,D);
+            beta(mu<=0.5) = (2*mu(mu<=0.5)).^(1/(disC+1));
+            beta(mu>0.5)  = (2-2*mu(mu>0.5)).^(-1/(disC+1));
+            beta = beta.*(-1).^randi([0,1],Nsize,D);
+            beta(rand(Nsize,D)<0.5) = 1;
+            beta(repmat(rand(Nsize,1)>proC,1,D)) = 1;
+            Offspring = [(Parent1+Parent2)/2+beta.*(Parent1-Parent2)/2,
+                         (Parent1+Parent2)/2-beta.*(Parent1-Parent2)/2];
+            Lower = repmat(lower,2*Nsize,1);
+            Upper = repmat(upper,2*Nsize,1);
+            Site  = rand(2*Nsize,D) < proM/D;
+            mu    = rand(2*Nsize,D);
+            temp  = Site & mu<=0.5;
+            Offspring       = min(max(Offspring,Lower),Upper);
+            Offspring(temp) = Offspring(temp)+(Upper(temp)-Lower(temp)).*((2.*mu(temp)+(1-2.*mu(temp)).*...
+                (1-(Offspring(temp)-Lower(temp))./(Upper(temp)-Lower(temp))).^(disM+1)).^(1/(disM+1))-1);
+            temp = Site & mu>0.5;
+            Offspring(temp) = Offspring(temp)+(Upper(temp)-Lower(temp)).*(1-(2.*(1-mu(temp))+2.*(mu(temp)-0.5).*...
+                (1-(Upper(temp)-Offspring(temp))./(Upper(temp)-Lower(temp))).^(disM+1)).^(1/(disM+1)));
+        end
+
+        function Offspring = GAhalf2_2(Parent,~)
+            Offspring = Parent - Parent.*(rand(size(Parent)))*0.25;
+        end
+
+        function Offspring = GAhalf1(Parent,~)
+            Offspring = Parent + Parent.*(rand(size(Parent)))*0.25;
+        end
+
+        function Offspring = GAhalf5(Parent,N)
+            fmin = 1.5*min(Parent,[],1) - 0.5*max(Parent,[],1);
+            fmax = 1.5*max(Parent,[],1) - 0.5*min(Parent,[],1);
+            Offspring = unifrnd(repmat(fmin,N,1),repmat(fmax,N,1));
+        end
+
+        function Con = calCon(PopuObj)
+            FrontNo = NDSort(PopuObj,inf);
+            Con     = sum(PopuObj,2);
+            Con     = FrontNo'*(max(Con)-min(Con)) + Con;
+        end
+
+        function [Offspring,idx] = EvaluateWithBudget(Problem,Decs,idx)
+            remain = Problem.maxFE - Problem.FE;
+            if remain <= 0
+                Offspring = [];
+                idx       = [];
+                return;
+            end
+            total = size(Decs,1);
+            if nargin < 3
+                if remain < total
+                    idx = randperm(total,remain);
+                else
+                    idx = 1:total;
+                end
+            else
+                idx = idx(:)';
+                if isempty(idx)
+                    Offspring = [];
+                    idx       = [];
+                    return;
+                end
+                if remain < length(idx)
+                    idx = idx(1:remain);
+                end
+            end
+            if isempty(idx)
+                Offspring = [];
+                return;
+            end
+            idx       = idx(:);
+            Offspring = Problem.Evaluation(Decs(idx,:));
+        end
+    end
 end
-
-
-
-
-
-
-
-
-
-
-
-function P = GAhalf_PF(obj,N)
-    P = [0:1/(N-1):1;1:-1/(N-1):0]';
-    P = P./repmat(sqrt(sum(P.^2,2)),1,size(P,2));
-    P = [P(:,ones(1,obj.Global.M-2)),P];
-    P = P./sqrt(2).^repmat([obj.Global.M-2,obj.Global.M-2:-1:0],size(P,1),1);
-end
-
-
-
-
-
-
-
-function Con = calCon(PopuObj)
-% Calculate the convergence of each solution
-
-    FrontNo = NDSort(PopuObj,inf);
-    Con     = sum(PopuObj,2);
-    Con     = FrontNo'*(max(Con)-min(Con)) + Con;
-end
-
-
-function Offspring = GAhalf2(Parent, N)
-	
-	Offspring = Parent - Parent.*(rand(size(Parent))*2 - 1)*0.5;
-end
-
-function output = sigmoid(x)
-    output =1./(1+exp(-x));
-end
-
-function output = swish(x)
-    output =x./(1+exp(-x));
-end
-function output = ELU(x)
-    output = 0.1*(exp(x)-1);
-% if x < 0
-%         output =x./(1+exp(-x));
-%     else
-%         output =x./(1+exp(-x));
-%     end
-end
-
-
-function output = FL(x)
-	if x < 0
-         output = x;
-     else
-         output = 0.5*x;
-     end
-end
-
-
-
-
-
-
-function Offspring = GAhalf4(Parent, N)
-	
-	Offspring = Parent - Parent.*(rand(size(Parent))-0.5);
-end
-
-
-function Offspring = GAhalf3(Parent,N1)
-
-    [proC,disC,proM,disM] = deal(1,20,1,20);
-	
-	
-	FrontNo    = NDSort(Parent,1) == 1;
-	%Populationtemp = Parent(FrontNo,:);
-	lower = min(Parent, [], 1)*0.01;
-	upper = max(Parent, [], 1)*10;
-	
-
-    Parent1 = Parent(1:floor(end/2),:);
-    Parent2 = Parent(floor(end/2)+1:floor(end/2)*2,:);
-    [N,D]   = size(Parent1);
-    Global  = GLOBAL.GetObj();
- 
-
-	%% Genetic operators for real encoding
-	% Simulated binary crossover
-	beta = zeros(N,D);
-	mu   = rand(N,D);
-	beta(mu<=0.5) = (2*mu(mu<=0.5)).^(1/(disC+1));
-	beta(mu>0.5)  = (2-2*mu(mu>0.5)).^(-1/(disC+1));
-	beta = beta.*(-1).^randi([0,1],N,D);
-	beta(rand(N,D)<0.5) = 1;
-	beta(repmat(rand(N,1)>proC,1,D)) = 1;
-	Offspring = [(Parent1+Parent2)/2+beta.*(Parent1-Parent2)/2
-				 (Parent1+Parent2)/2-beta.*(Parent1-Parent2)/2];
-	% Polynomial mutation
-	
-	Lower = repmat(lower,2*N,1);
-    Upper = repmat(upper,2*N,1);
-	Site  = rand(2*N,D) < proM/D;
-	mu    = rand(2*N,D);
-	temp  = Site & mu<=0.5;
-	Offspring       = min(max(Offspring,Lower),Upper);
-	Offspring(temp) = Offspring(temp)+(Upper(temp)-Lower(temp)).*((2.*mu(temp)+(1-2.*mu(temp)).*...
-					  (1-(Offspring(temp)-Lower(temp))./(Upper(temp)-Lower(temp))).^(disM+1)).^(1/(disM+1))-1);
-	temp = Site & mu>0.5; 
-	Offspring(temp) = Offspring(temp)+(Upper(temp)-Lower(temp)).*(1-(2.*(1-mu(temp))+2.*(mu(temp)-0.5).*...
-					  (1-(Upper(temp)-Offspring(temp))./(Upper(temp)-Lower(temp))).^(disM+1)).^(1/(disM+1)));
-
-
-end
-
-
-function Offspring = GAhalf2_2(Parent, N)
-	
-	Offspring = Parent - Parent.*(rand(size(Parent)))*0.25;
-end
-
-function Offspring = GAhalf1(Parent, N)
-	
-	Offspring = Parent + Parent.*(rand(size(Parent)))*0.25;
-end
-
-function Offspring = GAhalf5(Parent, N)
-%	 M = size(Parent, 2);
-	 fmin   = 1.5*min(Parent,[],1) - 0.5*max(Parent,[],1);
-     fmax   = 1.5*max(Parent,[],1) - 0.5*min(Parent,[],1);
-     Offspring = unifrnd(repmat(fmin,N,1),repmat(fmax,N,1));
-end
-
-
-
-
-
-
-
-
-function Population2 = GroupOptimization(Population,Div_V, R,Global)
-
-    
-	N            = length(Population);
-	OffDec       = Population(TournamentSelection(2,N,calCon(Population.objs))).decs;
-   NewObjs = GAhalf3(Population.objs, N);
-	Offspring_Convergence = ELU(NewObjs * R(:, Div_V));
-	NewObjs2 = GAhalf2_2(Population.objs, N);
-	Offspring_Convergence2 = ELU(NewObjs2 * R(:, Div_V));
-	temp = sqrt(OffDec(randperm(N),Div_V).*OffDec(randperm(N),Div_V));
-	temp2 = OffDec(:,Div_V);
-	a = randperm(N);
-	temp(temp>OffDec(a,Div_V)) = temp2(temp>OffDec(a,Div_V));
-	%OffDec(:,Div_V) = (temp) + ELU(Offspring_Convergence-Offspring_Convergence2);
-	OffDec(:,Div_V) = (temp) + (Offspring_Convergence-Offspring_Convergence2);
-   OffDec(:,Div_V) = min(max(OffDec(:,Div_V),repmat(Global.lower(Div_V),N,1)),repmat(Global.upper(Div_V),N,1));
-   Offspring    = INDIVIDUAL(OffDec);
-	Population2   = EnvironmentalSelection_A([Population,Offspring],N);
-    
-    
-
-	
-end
-
-
-function Population = SingleOptimization(Population,con_V, R,Global)
-   [N,D] = size(Population.decs);
-	OffDec = Population.decs;
-
-   NewObjs = GAhalf3(Population(randperm(N)).objs, N);
-
-	Offspring_Convergence = FL(NewObjs * R(:, con_V));
-	
-	NewObjs2 = GAhalf2_2(Population(randperm(N)).objs, N);
-
-	Offspring_Convergence2 = FL(NewObjs2 * R(:, con_V));
-   
-
-	OffDec(:,con_V) = OffDec(randperm(N),con_V) + (Offspring_Convergence - Offspring_Convergence2);
-	OffDec(:,con_V) = min(max(OffDec(:,con_V),repmat(Global.lower(con_V),N,1)),repmat(Global.upper(con_V),N,1));
-	
-
-	Offspring          = INDIVIDUAL(OffDec);
-
-    
-% 	allCon  = calCon([Population.objs;Offspring.objs]);
-% 	Con     = allCon(1:N);
-% 	newCon  = allCon(N+1:end);
-% 	updated = Con > newCon;
-    
-    
-    updated = sum(Offspring.objs <= Population.objs,2) >= Global.M;
-%    %updated     = sum(Offspring.objs ,2) <= sum(Population.objs,2);
-%    
-
-    
-
-
-	Population(updated) = Offspring(updated);
-
-
-end
-
--- PlatEMO/Algorithms/Multi-objective optimization/self/EAGOA/VariableClustering_A.m
+++ PlatEMO/Algorithms/Multi-objective optimization/self/EAGOA/VariableClustering_A.m
@@ -1,6 +1,6 @@
-function [PV,DV] = VariableClustering_A(Global,Population,nSel,nPer)
-% Detect the kind of each decision variable
-
+function [PV,DV] = VariableClustering_A(varargin)
+% Detect the kind of each decision variable (variant used by EAGOA)
+%
 %------------------------------- Copyright --------------------------------
 % Copyright (c) 2018-2019 BIMK Group. You are free to use the PlatEMO for
 % research purposes. All publications which use this platform or any code
@@ -10,56 +10,68 @@ function [PV,DV] = VariableClustering_A(Global,Population,nSel,nPer)
 % Computational Intelligence Magazine, 2017, 12(4): 73-87".
 %--------------------------------------------------------------------------
 
-    [N,D] = size(Population.decs);
-    ND    = NDSort(Population.objs,1) == 1;
-    fmin  = min(Population(ND).objs,[],1);
-    fmax  = max(Population(ND).objs,[],1);
-    if any(fmax==fmin)
-        fmax = ones(size(fmax));
-        fmin = zeros(size(fmin));
+    narginchk(4,4);
+
+    Population = varargin{2};
+    nSel       = varargin{3};
+    nPer       = varargin{4};
+
+    Problem = locateProblemHandle(varargin{1});
+
+    [PV,DV] = EAGOA.VariableClustering(Problem,Population,nSel,nPer);
+end
+
+function Problem = locateProblemHandle(arg1)
+% Locate the PROBLEM handle irrespective of whether a Global wrapper is used
+
+    if isa(arg1,'PROBLEM')
+        Problem = arg1;
+        return;
     end
-    
-    %% Calculate the proper values of each decision variable
-    Angle  = zeros(D,nSel);
-    RMSE   = zeros(D,nSel);
-    Sample = randi(N,1,nSel);
-    for i = 1 : D
-        drawnow();
-        % Generate several random solutions by perturbing the i-th dimension
-        Decs      = repmat(Population(Sample).decs,nPer,1);
-        Decs(:,i) = unifrnd(Global.lower(i),Global.upper(i),size(Decs,1),1);
-        newPopu   = INDIVIDUAL(Decs);
-        for j = 1 : nSel
-            % Normalize the objective values of the current perturbed solutions
-            Points = newPopu(j:nSel:end).objs;
-            Points = (Points-repmat(fmin,size(Points,1),1))./repmat(fmax-fmin,size(Points,1),1);
-            Points = Points - repmat(mean(Points,1),nPer,1);
-            % Calculate the direction vector of the determining line
-            [~,~,V] = svd(Points);
-            Vector  = V(:,1)'./norm(V(:,1)');
-            % Calculate the root mean square error
-            error = zeros(1,nPer);
-            for k = 1 : nPer
-                error(k) = norm(Points(k,:)-sum(Points(k,:).*Vector)*Vector);
+
+    if isobject(arg1)
+        if isprop(arg1,'problem')
+            Problem = arg1.problem;
+            if isa(Problem,'PROBLEM')
+                return;
             end
-            RMSE(i,j) = sqrt(sum(error.^2));
-            % Calculate the angle between the line and the hyperplane
-            normal     = ones(1,size(Vector,2));
-            sine       = abs(sum(Vector.*normal,2))./norm(Vector)./norm(normal);
-            Angle(i,j) = real(asin(sine)/pi*180);
+        end
+        if isprop(arg1,'Problem')
+            Problem = arg1.Problem;
+            if isa(Problem,'PROBLEM')
+                return;
+            end
+        end
+        if isprop(arg1,'pro')
+            Problem = arg1.pro;
+            if isa(Problem,'PROBLEM')
+                return;
+            end
         end
     end
-    
-    %% Detect the kind of each decision variable
-    VariableKind = (mean(RMSE,2)<1e-2)';
-    result       = kmeans(Angle,2)';
-    if any(result(VariableKind)==1) && any(result(VariableKind)==2)
-        if mean(mean(Angle(result==1&VariableKind,:))) > mean(mean(Angle(result==2&VariableKind,:)))
-            VariableKind = VariableKind & result==1;
-        else
-            VariableKind = VariableKind & result==2;
+
+    if isstruct(arg1)
+        if isfield(arg1,'problem')
+            Problem = arg1.problem;
+            if isa(Problem,'PROBLEM')
+                return;
+            end
+        end
+        if isfield(arg1,'Problem')
+            Problem = arg1.Problem;
+            if isa(Problem,'PROBLEM')
+                return;
+            end
+        end
+        if isfield(arg1,'pro')
+            Problem = arg1.pro;
+            if isa(Problem,'PROBLEM')
+                return;
+            end
         end
     end
-    PV = find(~VariableKind);
-    DV = find(VariableKind);
+
+    error('VariableClustering_A:InvalidInput', ...
+          ['First argument must be a PROBLEM instance or a wrapper that ', ...
+           'stores the problem handle in the field/property ''problem''.']);
 end
