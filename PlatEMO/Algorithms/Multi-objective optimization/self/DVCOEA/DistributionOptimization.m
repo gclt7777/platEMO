c1043feb7d5a4a0cc609587a89ff3fc15a830ddc
gclt7777 <zhaoxinman07@163.com>
2025-11-14 12:01:18 +0800

    Clamp DV indices in distribution optimizer

--- PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/DistributionOptimization.m
+++ PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/DistributionOptimization.m
@@ -1,13 +1,25 @@
-function Population = DistributionOptimization(Population,DV,CXV)
+function Population = DistributionOptimization(Problem,Population,DV,CXV)
 % Distribution optimization focusing on diversity-related variables
 
     if nargin < 3
         CXV = [];
     end
-    DV = setdiff(DV,unique(CXV));
+
+    PopDec = Population.decs;
+    D      = size(PopDec,2);
+
+    DV  = reshape(DV,1,[]);
+    CXV = reshape(CXV,1,[]);
+    DV  = DV(~isnan(DV));
+    CXV = CXV(~isnan(CXV));
+    DV  = unique(round(DV),'stable');
+    CXV = unique(round(CXV),'stable');
+    DV  = DV(~ismember(DV,CXV));
+    DV  = DV(DV>=1 & DV<=D);
     if isempty(DV)
         return;
     end
+
     N       = length(Population);
     Fitness = sum(Population.objs,2);
     Parents = Population(TournamentSelection(2,N,Fitness));
@@ -15,6 +27,6 @@ function Population = DistributionOptimization(Population,DV,CXV)
     parentDec = Population(randi(N,1,N)).decs;
     NewDec  = GA(parentDec);
     OffDec(:,DV) = NewDec(:,DV);
-    Offspring    = INDIVIDUAL(OffDec);
+    Offspring    = Problem.Evaluation(OffDec);
     Population   = EnvironmentalSelection([Population,Offspring],N);
 end
