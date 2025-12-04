e0d25af406c7c5cc5fb7640086d12786d282f6be
gclt7777 <zhaoxinman07@163.com>
2025-12-04 22:01:38 +0800

    Update DistributionOptimization.m

--- PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/DistributionOptimization.m
+++ PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/DistributionOptimization.m
@@ -1,45 +1,10 @@
-function Population = DistributionOptimization(Problem,Population,DV,CXV)
-% Distribution optimization focusing on diversity-related variables
+function Population = DistributionOptimization(Population,PV)
+% Distribution optimization
 
-    if nargin < 3
-        CXV = [];
-    end
-
-    PopDec = Population.decs;
-    D      = size(PopDec,2);
-
-    DV  = reshape(DV,1,[]);
-    CXV = reshape(CXV,1,[]);
-    PV  = [DV(~isnan(DV)),CXV(~isnan(CXV))];
-    PV  = unique(round(PV),'stable');
-    PV  = PV(PV>=1 & PV<=D);
-
-    if isempty(PV)
-        return;
-    end
-
-    N        = length(Population);
-    Fitness  = sum(Population.objs,2);
-    Parents  = Population(TournamentSelection(2,N,Fitness));
-    OffDec   = Parents.decs;
-
-    parentCount = max(2,2*ceil(N/2));
-    parentIdx   = randi(N,1,parentCount);
-    parentDec   = Population(parentIdx).decs;
-    gaOffspring = OperatorGA(Problem,parentDec);
-    if size(gaOffspring,1) < N
-        repFactor   = ceil(N/size(gaOffspring,1));
-        gaOffspring = repmat(gaOffspring,repFactor,1);
-    end
-    NewDec = gaOffspring(1:N,:);
-
-    maxCols = min([size(OffDec,2),size(NewDec,2),D]);
-    PV      = PV(PV<=maxCols);
-    if isempty(PV)
-        return;
-    end
-
-    OffDec(:,PV) = NewDec(:,PV);
-    Offspring    = Problem.Evaluation(OffDec);
-    Population   = EnvironmentalSelection([Population,Offspring],N);
+N            = length(Population);
+OffDec       = Population(TournamentSelection(2,N,sum(Population.objs,2))).decs;
+NewDec       = GA(Population(randi(N,1,N)).decs);
+OffDec(:,PV) = NewDec(:,PV);
+Offspring    = INDIVIDUAL(OffDec);
+Population   = EnvironmentalSelection([Population,Offspring],N);
 end
