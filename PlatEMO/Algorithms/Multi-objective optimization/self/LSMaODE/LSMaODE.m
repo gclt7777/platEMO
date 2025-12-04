f131b690573534373518d9ce4fab1457ce0b016d
gclt7777 <zhaoxinman07@163.com>
2025-12-04 20:00:45 +0800

    Handle GUI maxFE updates in LSMaODE

--- PlatEMO/Algorithms/Multi-objective optimization/self/LSMaODE/LSMaODE.m
+++ PlatEMO/Algorithms/Multi-objective optimization/self/LSMaODE/LSMaODE.m
@@ -1,5 +1,8 @@
-function LSMaODE(Global)
-% <algorithm> <L>
+classdef LSMaODE < ALGORITHM
+% <2022> <many> <real/integer> <large>
+% Large-scale many-objective differential evolution
+% mutationStrength --- 10  --- Step size controller in Gaussian mutation
+% proportion       --- 0.1 --- Proportion of solutions assigned to the local subpopulation
 
 %------------------------------- Reference --------------------------------
 % K. Zhang, C. Shen and G. G. Yen,
@@ -7,77 +10,110 @@ function LSMaODE(Global)
 %  IEEE Transactions on Cybernetics, 2022, doi: 10.1109/TCYB.2022.3178929.
 %--------------------------------------------------------------------------
 
-    %% Parameter setting
-    [mutationStrength,proportion] = Global.ParameterSet(10,0.1);
+    methods
+        function main(Algorithm,Problem)
+            %% Parameter setting
+            [mutationStrength,proportion] = Algorithm.ParameterSet(10,0.1);
+            if isempty(mutationStrength)
+                mutationStrength = 10;
+            end
+            if isempty(proportion)
+                proportion = 0.1;
+            end
 
-    %% Generate random population
-    Population = Global.Initialization();
-    lower      = Global.lower;
-    upper      = Global.upper;
+            %% Generate random population
+            Population = Problem.Initialization();
+            lower      = Problem.lower;
+            upper      = Problem.upper;
+            %% Use the current evaluation budget from the platform
+            %  Do not cache it too early in case the GUI updates maxFE just
+            %  before execution. The inner loops will respect this budget.
 
-    %% Optimization
-    while Global.NotTermination(Population)
-        localgroup      = floor(proportion*Global.N);
-        localPopulation = Population(1:localgroup);
-        L1list          = [];
-        for iter = 1 : 20 %#ok<NASGU>
-            for i = 1 : Global.N
-                if i <= localgroup
-                    for k = 1 : Global.D
-                        Parent         = Population(i);
-                        Offspring_dec  = Mutation(localgroup,i,k,Parent,lower,upper,mutationStrength,localPopulation,[]);
-                        Offspring      = INDIVIDUAL(Offspring_dec);
-                        mat_Population = [Parent,Offspring];
-                        [FrontNo,MaxFNo] = NDSort(mat_Population.objs,mat_Population.cons,2);
-                        if MaxFNo ~= 1
-                            Population(i) = mat_Population(FrontNo==1);
-                        else
-                            temp_Population    = localPopulation;
-                            temp_Population(i) = [];
-                            off_dom_count = CalDomcount(temp_Population.objs,Offspring.objs);
-                            Par_dom_count = CalDomcount(temp_Population.objs,Parent.objs);
-                            if off_dom_count < Par_dom_count
-                                Population(i) = Offspring;
-                            elseif off_dom_count == Par_dom_count
-                                temp_localPopulation    = localPopulation;
-                                temp_localPopulation(i) = [];
-                                [Par_MED,off_MED] = MED(temp_localPopulation,Parent,Offspring);
-                                if off_MED > Par_MED
+            %% Optimization
+            while Algorithm.NotTerminated(Population)
+                maxBudget  = Problem.maxFE;
+                stopSearch = false;
+                localgroup      = floor(proportion*Problem.N);
+                localPopulation = Population(1:localgroup);
+                L1list          = [];
+                for iter = 1 : 20 %#ok<NASGU>
+                    if stopSearch
+                        break;
+                    end
+                    for i = 1 : Problem.N
+                        stopSearch = Problem.FE >= maxBudget;
+                        if stopSearch
+                            break;
+                        end
+                        if i <= localgroup
+                            for k = 1 : Problem.D
+                                stopSearch = Problem.FE >= maxBudget;
+                                if stopSearch
+                                    break;
+                                end
+                                Parent         = Population(i);
+                                Offspring_dec  = Mutation(localgroup,i,k,Parent,lower,upper,mutationStrength,localPopulation,[]);
+                                Offspring      = Problem.Evaluation(Offspring_dec);
+                                mat_Population = [Parent,Offspring];
+                                [FrontNo,MaxFNo] = NDSort(mat_Population.objs,mat_Population.cons,2);
+                                if MaxFNo ~= 1
+                                    Population(i) = mat_Population(FrontNo==1);
+                                else
+                                    temp_Population    = localPopulation;
+                                    temp_Population(i) = [];
+                                    off_dom_count = CalDomcount(temp_Population.objs,Offspring.objs);
+                                    Par_dom_count = CalDomcount(temp_Population.objs,Parent.objs);
+                                    if off_dom_count < Par_dom_count
+                                        Population(i) = Offspring;
+                                    elseif off_dom_count == Par_dom_count
+                                        temp_localPopulation    = localPopulation;
+                                        temp_localPopulation(i) = [];
+                                        [Par_MED,off_MED] = MED(temp_localPopulation,Parent,Offspring);
+                                        if off_MED > Par_MED
+                                            Population(i) = Offspring;
+                                        end
+                                    end
+                                end
+                            end
+                        end
+
+                        if i == localgroup+1
+                            [GFrontNo,~] = NDSort(Population.objs,Population.cons,Problem.N);
+                            List   = 1 : Problem.N;
+                            L1list = List(GFrontNo==1);
+                        end
+
+                        if i > localgroup
+                            stopSearch = Problem.FE >= maxBudget;
+                            if stopSearch
+                                break;
+                            end
+                            Parent         = Population(i);
+                            Offspring_dec  = Mutation(localgroup,i,[],Parent,lower,upper,mutationStrength,Population,L1list);
+                            Offspring      = Problem.Evaluation(Offspring_dec);
+                            mat_Population = [Parent,Offspring];
+                            [FrontNo,MaxFNo] = NDSort(mat_Population.objs,mat_Population.cons,2);
+                            if MaxFNo ~= 1
+                                Population(i) = mat_Population(FrontNo==1);
+                            else
+                                temp_Population    = Population;
+                                temp_Population(i) = [];
+                                off_dom_count = CalDomcount(temp_Population.objs,Offspring.objs);
+                                Par_dom_count = CalDomcount(temp_Population.objs,Parent.objs);
+                                if off_dom_count < Par_dom_count
                                     Population(i) = Offspring;
+                                elseif off_dom_count == Par_dom_count
+                                    [Par_MED,off_MED] = MED(temp_Population,Parent,Offspring);
+                                    if off_MED > Par_MED
+                                        Population(i) = Offspring;
+                                    end
                                 end
                             end
                         end
                     end
                 end
-
-                if i == localgroup+1
-                    [GFrontNo,~] = NDSort(Population.objs,Population.cons,Global.N);
-                    List   = 1 : Global.N;
-                    L1list = List(GFrontNo==1);
-                end
-
-                if i > localgroup
-                    Parent         = Population(i);
-                    Offspring_dec  = Mutation(localgroup,i,[],Parent,lower,upper,mutationStrength,Population,L1list);
-                    Offspring      = INDIVIDUAL(Offspring_dec);
-                    mat_Population = [Parent,Offspring];
-                    [FrontNo,MaxFNo] = NDSort(mat_Population.objs,mat_Population.cons,2);
-                    if MaxFNo ~= 1
-                        Population(i) = mat_Population(FrontNo==1);
-                    else
-                        temp_Population    = Population;
-                        temp_Population(i) = [];
-                        off_dom_count = CalDomcount(temp_Population.objs,Offspring.objs);
-                        Par_dom_count = CalDomcount(temp_Population.objs,Parent.objs);
-                        if off_dom_count < Par_dom_count
-                            Population(i) = Offspring;
-                        elseif off_dom_count == Par_dom_count
-                            [Par_MED,off_MED] = MED(temp_Population,Parent,Offspring);
-                            if off_MED > Par_MED
-                                Population(i) = Offspring;
-                            end
-                        end
-                    end
+                if stopSearch
+                    break;
                 end
             end
         end
