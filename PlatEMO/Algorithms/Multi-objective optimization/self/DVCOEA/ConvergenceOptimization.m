042d6eb756ca2f066b6dc56a03ec6084bf8f3d0c
gclt7777 <zhaoxinman07@163.com>
2025-12-04 22:00:43 +0800

    Update ConvergenceOptimization.m

--- PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/ConvergenceOptimization.m
+++ PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/ConvergenceOptimization.m
@@ -1,27 +1,15 @@
-function Population = ConvergenceOptimization(Problem,Population,CVgroup)
-% Convergence optimization for a group of convergence-related variables
+function [ Offspring ] = ConvergenceOptimization(Population,CVgroup )
+% 对spop中的CVgroup部分决策变量做优化
+[N,D] = size(Population.decs);
+Con   = sum(Population.objs);
+% Select parents
+MatingPool = TournamentSelection(2,2*N,Con);
+% Generate offsprings
+OffDec = Population.decs;
+NewDec = DE(Population.decs,Population(MatingPool(1:end/2)).decs,...
+    Population(MatingPool(end/2+1:end)).decs,...
+    {1,0.5,D/length(CVgroup)/2,20});
 
-    PopDec = Population.decs;
-    N      = size(PopDec,1);
-    D      = size(PopDec,2);
-    CVgroup = reshape(CVgroup,1,[]);
-    CVgroup = CVgroup(~isnan(CVgroup));
-    CVgroup = unique(round(CVgroup),'stable');
-    CVgroup = CVgroup(CVgroup>=1 & CVgroup<=D);
-    if isempty(CVgroup)
-        return;
-    end
-    % Select parents
-    Con         = sum(Population.objs,2);
-    MatingPool  = TournamentSelection(2,2*N,Con);
-    rate        = max(1,floor(D/length(CVgroup)/2));
-    OffDec      = PopDec;
-    NewDec      = OperatorDE(Problem,PopDec,...
-        Population(MatingPool(1:end/2)).decs,...
-        Population(MatingPool(end/2+1:end)).decs,...
-        {1,0.5,rate,20});
-    OffDec(:,CVgroup) = NewDec(:,CVgroup);
-    Offspring         = Problem.Evaluation(OffDec);
-    better            = all(Offspring.objs<=Population.objs,2) & any(Offspring.objs<Population.objs,2);
-    Population(better) = Offspring(better);
+OffDec(:,CVgroup) = NewDec(:,CVgroup);
+Offspring = INDIVIDUAL(OffDec);
 end
