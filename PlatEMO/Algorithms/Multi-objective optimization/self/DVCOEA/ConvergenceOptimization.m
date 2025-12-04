b5b06eb83188a40df10f6311f9d17951ab6b8f9e
gclt7777 <zhaoxinman07@163.com>
2025-12-04 22:20:12 +0800

    Guard convergence optimization for missing variables

--- PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/ConvergenceOptimization.m
+++ PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/ConvergenceOptimization.m
@@ -1,15 +1,24 @@
-function [ Offspring ] = ConvergenceOptimization(Population,CVgroup )
+function [ Offspring ] = ConvergenceOptimization(Problem,Population,CVgroup)
 % 对spop中的CVgroup部分决策变量做优化
 [N,D] = size(Population.decs);
-Con   = sum(Population.objs);
+if isempty(CVgroup)
+    Offspring = Population;
+    return;
+end
+CVgroup = CVgroup(CVgroup<=D);
+if isempty(CVgroup)
+    Offspring = Population;
+    return;
+end
+Con   = sum(Population.objs,2);
 % Select parents
 MatingPool = TournamentSelection(2,2*N,Con);
 % Generate offsprings
 OffDec = Population.decs;
 NewDec = DE(Population.decs,Population(MatingPool(1:end/2)).decs,...
     Population(MatingPool(end/2+1:end)).decs,...
-    {1,0.5,D/length(CVgroup)/2,20});
+    {1,0.5,max(1,D/length(CVgroup)/2),20});
 
 OffDec(:,CVgroup) = NewDec(:,CVgroup);
-Offspring = INDIVIDUAL(OffDec);
+Offspring = Problem.Evaluation(OffDec);
 end
