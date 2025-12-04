6e83df746e8170b963de64cc62b22d6dec9af36b
gclt7777 <zhaoxinman07@163.com>
2025-12-04 22:15:59 +0800

    Fix DVCOEA evaluation calls

--- PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/ConvergenceOptimization.m
+++ PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/ConvergenceOptimization.m
@@ -1,7 +1,7 @@
-function [ Offspring ] = ConvergenceOptimization(Population,CVgroup )
+function [ Offspring ] = ConvergenceOptimization(Problem,Population,CVgroup)
 % 对spop中的CVgroup部分决策变量做优化
 [N,D] = size(Population.decs);
-Con   = sum(Population.objs);
+Con   = sum(Population.objs,2);
 % Select parents
 MatingPool = TournamentSelection(2,2*N,Con);
 % Generate offsprings
@@ -11,5 +11,5 @@ NewDec = DE(Population.decs,Population(MatingPool(1:end/2)).decs,...
     {1,0.5,D/length(CVgroup)/2,20});
 
 OffDec(:,CVgroup) = NewDec(:,CVgroup);
-Offspring = INDIVIDUAL(OffDec);
+Offspring = Problem.Evaluation(OffDec);
 end
