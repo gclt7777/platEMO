1d8898ce2a7fff5cd4d33c6b8e885389ce3524f4
gclt7777 <zhaoxinman07@163.com>
2025-12-04 21:42:06 +0800

    Refactor ConvergenceOptimization function parameters

--- PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/ConvergenceOptimization.m
+++ PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/ConvergenceOptimization.m
@@ -1,21 +1,27 @@
-function Population = ConvergenceOptimization(Population,CVgroup)
+function Population = ConvergenceOptimization(Problem,Population,CVgroup)
 % Convergence optimization for a group of convergence-related variables
 
+    PopDec = Population.decs;
+    N      = size(PopDec,1);
+    D      = size(PopDec,2);
+    CVgroup = reshape(CVgroup,1,[]);
+    CVgroup = CVgroup(~isnan(CVgroup));
+    CVgroup = unique(round(CVgroup),'stable');
+    CVgroup = CVgroup(CVgroup>=1 & CVgroup<=D);
     if isempty(CVgroup)
         return;
     end
-    N = length(Population);
-    D = size(Population.decs,2);
     % Select parents
     Con         = sum(Population.objs,2);
     MatingPool  = TournamentSelection(2,2*N,Con);
-    OffDec      = Population.decs;
-    NewDec      = DE(Population.decs,...
+    rate        = max(1,floor(D/length(CVgroup)/2));
+    OffDec      = PopDec;
+    NewDec      = OperatorDE(Problem,PopDec,...
         Population(MatingPool(1:end/2)).decs,...
         Population(MatingPool(end/2+1:end)).decs,...
-        {1,0.5,D/length(CVgroup)/2,20});
+        {1,0.5,rate,20});
     OffDec(:,CVgroup) = NewDec(:,CVgroup);
-    Offspring         = INDIVIDUAL(OffDec);
+    Offspring         = Problem.Evaluation(OffDec);
     better            = all(Offspring.objs<=Population.objs,2) & any(Offspring.objs<Population.objs,2);
     Population(better) = Offspring(better);
 end
