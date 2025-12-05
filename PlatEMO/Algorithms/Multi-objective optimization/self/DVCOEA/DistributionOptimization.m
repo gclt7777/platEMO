11aebdc9bfeaff5055299469ec742f814fcda8e1
gclt7777 <zhaoxinman07@163.com>
2025-12-05 12:37:44 +0800

    Tighten distribution variable bounds

--- PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/DistributionOptimization.m
+++ PlatEMO/Algorithms/Multi-objective optimization/self/DVCOEA/DistributionOptimization.m
@@ -1,10 +1,22 @@
-function Population = DistributionOptimization(Population,PV)
+function Population = DistributionOptimization(Problem,Population,PV,CXV)
 % Distribution optimization
 
-N            = length(Population);
-OffDec       = Population(TournamentSelection(2,N,sum(Population.objs,2))).decs;
-NewDec       = GA(Population(randi(N,1,N)).decs);
-OffDec(:,PV) = NewDec(:,PV);
-Offspring    = INDIVIDUAL(OffDec);
-Population   = EnvironmentalSelection([Population,Offspring],N);
+if nargin < 3
+    CXV = [];
+end
+
+N              = length(Population);
+OffDec         = Population(TournamentSelection(2,N,sum(Population.objs,2))).decs;
+NewDec         = GA(Population(randi(N,1,N)).decs);
+maxDecColumns  = min(size(OffDec,2),size(NewDec,2));
+PV             = unique([PV,CXV]);
+PV             = PV(PV >= 1 & PV <= maxDecColumns & isfinite(PV));
+
+if isempty(PV) || maxDecColumns == 0
+    return;
+end
+
+OffDec(:,PV)   = NewDec(:,PV);
+Offspring      = Problem.Evaluation(OffDec);
+Population     = EnvironmentalSelection([Population,Offspring],N);
 end
