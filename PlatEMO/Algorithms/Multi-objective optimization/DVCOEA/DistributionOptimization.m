e64ac3dceda9f5be33fd84a6972a5947e237f161
gclt7777 <zhaoxinman07@163.com>
2025-12-08 10:44:33 +0800

    Use OperatorGA for DVCOEA distribution step

--- PlatEMO/Algorithms/Multi-objective optimization/DVCOEA/DistributionOptimization.m
+++ PlatEMO/Algorithms/Multi-objective optimization/DVCOEA/DistributionOptimization.m
@@ -5,12 +5,18 @@ function Population = DistributionOptimization(Problem,Population,DV,CXV)
         CXV = [];
     end
     PV = unique([DV,CXV]);
+    PV = PV(PV>=1 & PV<=Problem.D);
+    if isempty(PV)
+        return;
+    end
 
     N            = length(Population);
-OffDec       = Population(TournamentSelection(2,N,sum(Population.objs,2))).decs;
-NewDec       = GA(Population(randi(N,1,N)).decs);
-OffDec(:,PV) = NewDec(:,PV);
-Offspring    = Problem.Evaluation(OffDec);
-Population   = EnvironmentalSelection([Population,Offspring],N);
+    OffDec       = Population(TournamentSelection(2,N,sum(Population.objs,2))).decs;
+    parentDec    = Population(randi(N,1,2*ceil(N/2))).decs;
+    NewDec       = OperatorGA(Problem,parentDec,{1,20,1,20});
+    NewDec       = NewDec(1:N,:);
+    OffDec(:,PV) = NewDec(:,PV);
+    Offspring    = Problem.Evaluation(OffDec);
+    Population   = EnvironmentalSelection([Population,Offspring],N);
 end
 
