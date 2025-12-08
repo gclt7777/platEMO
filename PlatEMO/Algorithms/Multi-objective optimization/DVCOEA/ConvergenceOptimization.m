5437b76ec2a80264eeceaf3cbe431a64f294932d
gclt7777 <zhaoxinman07@163.com>
2025-12-08 10:36:06 +0800

    Fix convergence optimization offspring generation

--- PlatEMO/Algorithms/Multi-objective optimization/DVCOEA/ConvergenceOptimization.m
+++ PlatEMO/Algorithms/Multi-objective optimization/DVCOEA/ConvergenceOptimization.m
@@ -1,15 +1,15 @@
 function [ Offspring ] = ConvergenceOptimization(Problem,Population,CVgroup )
-Offspring = Problem.Evaluation(OffDec);
 [N,D] = size(Population.decs);
 Con   = sum(Population.objs);
 % Select parents
 MatingPool = TournamentSelection(2,2*N,Con);
-% Generate offsprings
+% Generate offsprings on the selected convergence-related variables
 OffDec = Population.decs;
-NewDec = DE(Population.decs,Population(MatingPool(1:end/2)).decs,...
+NewDec = OperatorDE(Problem,Population.decs,...
+    Population(MatingPool(1:end/2)).decs,...
     Population(MatingPool(end/2+1:end)).decs,...
     {1,0.5,D/length(CVgroup)/2,20});
 
 OffDec(:,CVgroup) = NewDec(:,CVgroup);
-Offspring = INDIVIDUAL(OffDec);
+Offspring = Problem.Evaluation(OffDec);
 end
